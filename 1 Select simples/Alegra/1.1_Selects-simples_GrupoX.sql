7024f1c919c3c48873dc292434d1e2deb8eca4a3
lucia <lucia.tecles@idesie.com>
2024-02-21 20:27:58 +0100

    Update 1.1_Selects-simples_GrupoX.sql

--- 1 Select simples/Alegra/1.1_Selects-simples_GrupoX.sql
+++ 1 Select simples/Alegra/1.1_Selects-simples_GrupoX.sql
@@ -24,36 +24,52 @@ from components;
 Datos de la tabla component_types
 */
 select *
-from component_types;
+from 
+    component_types;
 
 
 /* 5
 Id, nombre de los facilities
 */
-select ID, name
-from facilities;
+select 
+    ID, 
+    name
+from 
+    facilities;
 
 
 /* 6
 Nombre, elevación e id del facility de las plantas
 */
-select name, elevation, facilityid
-from floors;
+select 
+    name, 
+    elevation, 
+    facilityid
+from 
+    floors;
 
 
 /* 7
 Nombre, area bruta, volumen de los espacios
 */
-select name, grossarea, volume
-from spaces;
+select 
+    name, 
+    grossarea, 
+    volume
+from 
+    spaces;
 
 
 /* 8
 Nombre, vida útil de los tipos de componentes del facility 1
 */
-select name, expectedlife
-from component_types
-where facilityid = 1;
+select 
+    name, 
+    expectedlife
+from 
+    component_types
+where 
+    facilityid = 1;
 
 
 /* 9
@@ -61,83 +77,132 @@ Nombre de los espacios de la Planta 1 del facility 1
 */
 /*Previamente se consulta cuál es el floorid
 listando los */
-select *
-from facilities;
+select 
+    *
+from 
+    facilities;
+
 -- El ID = 1 es el Colegio Alegra
 
-select *
-from floors
-where facilityid = 1;
+select 
+    *
+from 
+    floors
+where 
+    facilityid = 1;
+
 -- La planta 1 es el ID = 1
 
-select name
-from spaces
-where floorid = 1;
+select 
+    name
+from 
+    spaces
+where 
+    floorid = 1;
 
 
 /* 10
 Nombre, número de modelo del tipo de componente con id = 60
 */
-select name, modelnumber
-from component_types
-where id = 60;
+select 
+    name, 
+    modelnumber
+from 
+    component_types
+where 
+    id = 60;
 
 
 /* 11
 Nombre y fecha de instalación de los componentes del espacio 60 ordenados descendentemente por la fecha de instalación
 */
-select name, installatedon
-from components
-where spaceid = 60
-order by installatedon desc;
+select 
+    name, 
+        installatedon
+from 
+    components
+where 
+    spaceid = 60
+order by 
+    installatedon desc;
 
 
 /* 12
 Listar las distintas fechas de instalación de los componentes del facility 1 ordenados descendentemente.
 */
-select installatedon
-from components
-where facilityid = 1
-order by installatedon desc;
+select 
+    installatedon
+from 
+    components
+where 
+    facilityid = 1
+order by 
+    installatedon desc;
 
 
 /* 13
 Listar los distintos GUIDs de los componentes del facility 1 ordenados ascendentemente por fecha de garantía.
 */
-select distinct EXTERNALIDENTIFIER
-from components
-where facilityid = 1
-order by warrantystarton asc;
+select distinct 
+    EXTERNALIDENTIFIER
+from 
+    components
+where 
+    facilityid = 1
+order by 
+    warrantystarton asc;
 
 
 /* 14
 Id, código de activo, GUID, número de serie y nombre de los componentes cuyo spaceid está entre 10 y 27 inclusive
 ordenados por id de espacio descendentemente.
 */
-select ID, ASSETIDENTIFIER, EXTERNALIDENTIFIER, SERIALNUMBER, NAME
-from COMPONENTS
-where SPACEID >= 10 AND SPACEID <= 27
-order by spaceid desc;
+select 
+    ID, 
+    ASSETIDENTIFIER, 
+    EXTERNALIDENTIFIER, 
+    SERIALNUMBER, 
+    NAME
+from 
+    COMPONENTS
+where 
+    SPACEID >= 10 
+        AND 
+    SPACEID <= 27
+order by 
+    spaceid desc;
 
 
 /* 15
 Id, código de activo, GUID, número de serie y nombre de los componentes del facility 1 
 ordenados por código de activo descendentemente.
 */
-select ID, ASSETIDENTIFIER, EXTERNALIDENTIFIER, SERIALNUMBER, NAME
-from COMPONENTS
-where FACILITYID = 1
-order by ASSETIDENTIFIER desc;
+select 
+    ID, 
+    ASSETIDENTIFIER, 
+    EXTERNALIDENTIFIER, 
+    SERIALNUMBER, 
+    NAME
+from 
+    COMPONENTS
+where 
+    FACILITYID = 1
+order by 
+    ASSETIDENTIFIER desc;
 
 
 /* 16
 Códigos de activo de los componentes del espacio con id 21
 ordenados por código de activo descendentemente.
 */
-select ASSETIDENTIFIER
-from COMPONENTS
-where SPACEID = 21
-order by ASSETIDENTIFIER desc;
+select 
+    ASSETIDENTIFIER
+from 
+    COMPONENTS
+where   
+    SPACEID = 21
+order by 
+    ASSETIDENTIFIER desc;
 
 
 /* 17
@@ -145,10 +210,20 @@ Las distintas fechas de instalación de los componentes
 de los espacios con id 10, 12, 16, 19 
 ordenadas descendentemente.
 */
-select distinct INSTALLATEDON
-from COMPONENTS
-where SPACEID = 10 OR SPACEID = 12 OR SPACEID = 16 OR SPACEID = 19
-order by INSTALLATEDON desc;
+select distinct 
+    INSTALLATEDON
+from 
+    COMPONENTS
+where 
+    SPACEID = 10 
+        OR 
+    SPACEID = 12 
+        OR 
+    SPACEID = 16 
+        OR 
+    SPACEID = 19
+order by 
+    INSTALLATEDON desc;
 
 
 /* 18
@@ -156,20 +231,34 @@ Nombre, volumen, de los espacios
 cuyo volumen es mayor a 90 de floorid = 1
 ordenados por volumen descendentemente
 */
-select name, volume
-from spaces
-where (volume > 90) and (floorid = 1)
-order by volume desc;
+select 
+    name, 
+    volume
+from 
+    spaces
+where 
+    (volume > 90) 
+        and 
+    (floorid = 1)
+order by 
+    volume desc;
 
 
 /* 19
 Nombre, volumen de los espacios
 cuyo volumen es mayor a 6 y menor a 9 de la planta con id = 1
 */
-select name, volume
-from spaces
-where (volume > 6 and volume < 9) and floorid = 1
-order by volume desc;
+select 
+    name, 
+    volume
+from 
+    spaces
+where 
+    (volume > 6 and volume < 9) 
+        and 
+    floorid = 1
+order by 
+    volume desc;
 
 
 /* 20
@@ -182,11 +271,14 @@ select
     assetidentifier,
     serialnumber,
     spaceid
-from components
+from 
+    components
 where
     facilityid = 1
-    and spaceid is null
-order by assetidentifier desc;
+        and 
+    spaceid is null
+order by 
+    assetidentifier desc;
 
 
 /* 21
@@ -208,9 +300,12 @@ Lista de espacios que su segunda letra es una 's' donde floorid = 1
 select
     name,
     floorid
-from spaces
-where floorid = 1
-    and substr(name, 2, 1) = 's';
+from 
+    spaces
+where 
+    floorid = 1
+        and 
+    substr(name, 2, 1) = 's';
 
 
 /* 24
@@ -219,10 +314,14 @@ donde el nombre contiene el texto 'con'
 y no tienen vida útil indicada o fecha de garantía de las partes
 */
 select *
-from component_types
-where facilityid = 1
-    and name like '%con%'
-    and (expectedlife is null or warrantyguarantorparts is null);
+from 
+    component_types
+where 
+    facilityid = 1
+        and 
+    name like '%con%'
+        and 
+    (expectedlife is null or warrantyguarantorparts is null);
 
 
 /* 25
@@ -242,7 +341,8 @@ select
         else
             'MEDIO'
     end
-from spaces;
+from 
+    spaces;
 
 
 /* 26
@@ -250,59 +350,86 @@ Nombre, fecha de instalación, fecha de garantia
 de los componentes del facility 1
 que tienen fecha de garantia
 */
-select name, installatedon, warrantystarton
-from components
-where facilityid = 1
-    and warrantystarton is not null;
+select 
+    name,  
+    installatedon, 
+    warrantystarton
+from 
+    components
+where 
+    facilityid = 1
+        and 
+    warrantystarton is not null;
 
 
 /* 27
 Lista de nombres de espacio que su id no es 4, 9, ni 19
 del floorid 1
 */
-select name
-from spaces
-where floorid = 1
-    and id not in (4, 9, 19);
+select 
+    name
+from 
+    spaces
+where 
+    floorid = 1
+        and 
+    id not in (4, 9, 19);
 
 
 /* 28
 Lista de espacios que no son Aula del floorid = 1
 */
-select *
-from spaces
-where floorid = 1
-    and lower(name) not like '%aula%';
+select 
+    *
+from 
+    spaces
+where 
+    floorid = 1
+        and 
+    lower(name) not like '%aula%';
 
 
 /* 29
 Lista de los tipos de componentes que tienen duracion de la garantia de las partes
 del facility 1
 */
-select name, warrantydurationparts, facilityid
-from component_types
+select 
+    name, 
+    warrantydurationparts, 
+    facilityid
+from 
+    component_types
 where
     warrantydurationparts is not null
-    and facilityid = 1;
+        and 
+    facilityid = 1;
 
 
 /* 30
 Lista de los tipos de componentes que no tiene el coste de repuesto
 del facility 1
 */
-select name, replacementcost
-from component_types
-where replacementcost is null;
+select 
+    name, 
+    replacementcost
+from 
+    component_types
+where 
+    replacementcost is null;
 
 
 /* 31
 Lista de los tipos de componentes que tienen en el nombre un guión bajo
 del facility 1
 */
-select name
-from component_types
-where name like '%\_%' escape '\'
-and facilityid = 1;
+select 
+    name
+from 
+    component_types
+where 
+    name like '%\_%' escape '\'
+        and 
+    facilityid = 1;
 
 
 --

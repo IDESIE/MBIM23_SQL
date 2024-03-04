f8d3be1599fbece01e92c8ff8b2cbea8af76b9ae
Jaime <jaime.lorenzo@idesie.com>
2024-03-04 17:18:42 +0100

    Update 1.1_Selects-simples_GrupoX.sql

--- 1 Select simples/Alegra/1.1_Selects-simples_GrupoX.sql
+++ 1 Select simples/Alegra/1.1_Selects-simples_GrupoX.sql
@@ -4,42 +4,26 @@
 /* 1
 Describir la tabla floors
 */
-desc floors;
-
 
 /* 2
 Describir la tabla spaces
 */
-desc spaces;
-
 
 /* 3
 Datos de la tabla components
 */
-select *
-from components;
-
 
 /* 4
 Datos de la tabla component_types
 */
-select *
-from component_types;
-
 
 /* 5
 Id, nombre de los facilities
 */
-select ID, name
-from facilities;
-
 
 /* 6
 Nombre, elevación e id del facility de las plantas
 */
-select name, elevation, facilityid
-from floors;
-
 
 /* 7
 Nombre, area bruta, volumen de los espacios
@@ -61,19 +45,6 @@ Nombre de los espacios de la Planta 1 del facility 1
 */
 /*Previamente se consulta cuál es el floorid
 listando los */
-select *
-from facilities;
--- El ID = 1 es el Colegio Alegra
-
-select *
-from floors
-where facilityid = 1;
--- La planta 1 es el ID = 1
-
-select name
-from spaces
-where floorid = 1;
-
 
 /* 10
 Nombre, número de modelo del tipo de componente con id = 60
@@ -86,11 +57,6 @@ where id = 60;
 /* 11
 Nombre y fecha de instalación de los componentes del espacio 60 ordenados descendentemente por la fecha de instalación
 */
-select name, installatedon
-from components
-where spaceid = 60
-order by installatedon desc;
-
 
 /* 12
 Listar las distintas fechas de instalación de los componentes del facility 1 ordenados descendentemente.
@@ -104,31 +70,16 @@ order by installatedon desc;
 /* 13
 Listar los distintos GUIDs de los componentes del facility 1 ordenados ascendentemente por fecha de garantía.
 */
-select distinct EXTERNALIDENTIFIER
-from components
-where facilityid = 1
-order by warrantystarton asc;
-
 
 /* 14
 Id, código de activo, GUID, número de serie y nombre de los componentes cuyo spaceid está entre 10 y 27 inclusive
 ordenados por id de espacio descendentemente.
 */
-select ID, ASSETIDENTIFIER, EXTERNALIDENTIFIER, SERIALNUMBER, NAME
-from COMPONENTS
-where SPACEID >= 10 AND SPACEID <= 27
-order by spaceid desc;
-
 
 /* 15
 Id, código de activo, GUID, número de serie y nombre de los componentes del facility 1 
 ordenados por código de activo descendentemente.
 */
-select ID, ASSETIDENTIFIER, EXTERNALIDENTIFIER, SERIALNUMBER, NAME
-from COMPONENTS
-where FACILITYID = 1
-order by ASSETIDENTIFIER desc;
-
 
 /* 16
 Códigos de activo de los componentes del espacio con id 21

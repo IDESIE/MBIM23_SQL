63f5104f2aa6d70e4f3d65f58c527539f0608ba5
enriqueleante <enrique.leante@idesie.com>
2024-03-10 22:58:09 +0100

    Update 3.1_DML_GrupoX.sql

--- 3 DML/Alegra/3.1_DML_GrupoX.sql
+++ 3 DML/Alegra/3.1_DML_GrupoX.sql
@@ -89,7 +89,12 @@ WHERE EXISTS (SELECT 1
 Modificar la fecha de garantia para que sea igual a la fecha de instalación
 para todo componente que sea un grifo o lavabo del facility 1.
 */
-
+update components
+set warrantystarton = installatedon
+where id in (select id
+    from components
+    where facilityid = 1 and
+    (lower(name) like '%lavabo%' or lower(name) like '%grifo%'));
 /* 5
 Anonimizar los datos personales: nombre, apellido, email, teléfono de los contactos
 */
